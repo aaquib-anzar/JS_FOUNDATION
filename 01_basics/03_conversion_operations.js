95009e08ff7268e84f064f8b71ec1c502eb06118
Aaquib Anzar <48081691+aaquib-anzar@users.noreply.github.com>
2023-11-29 11:32:58 +0000

    conversion of datatype

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,15 @@
+let score = undefined
+let newScore = Number(score)
+
+console.log(score)
+console.log(typeof(score))
+console.log(newScore)
+console.log(typeof(newScore))
+
+/*
+33 number => 33 number
+"33" string => 33 number
+"Aaquib" string => NaN (not a number) number
+null object => 0 number
+undefined undefined => NaN number
+*/
