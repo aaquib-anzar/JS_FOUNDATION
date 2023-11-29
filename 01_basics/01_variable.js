3e1648b6448d7c697b2045ef3be7748e206afe9e
Aaquib Anzar <48081691+aaquib-anzar@users.noreply.github.com>
2023-11-29 10:20:50 +0000

    variables done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const account_id = 5865476 //fixed nevr changed
+let account_email = "aaquib@gmail.com" //can be changed always preferred.
+var account_password = "12568" //can be changed , prefer not to use because of scope issue
+account_city = "Ghaziabad" //without any let const can also be declared.
+account_password = "8965"
+let account_state
+console.log(account_state)
+
+console.table([account_id,account_email,account_password,account_city])
+
