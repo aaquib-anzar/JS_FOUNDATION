0755e8a31e04e6db93fab65c2c997806d8967f7e
Aaquib Anzar <48081691+aaquib-anzar@users.noreply.github.com>
2023-11-29 11:04:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code as newer version.
+
+alert()
+
+/* int
+bigint
+boolean
+string
+null -> standalone value  type = object
+undefined type = undefined
+symbol -> uniqueness */
+
+//object
+
+console.log(typeof("Aaquib"));
