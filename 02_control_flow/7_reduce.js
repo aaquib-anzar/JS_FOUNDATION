baca14c9261e09225f604f0e3c569bac71f7928d
Aaquib Anzar <48081691+aaquib-anzar@users.noreply.github.com>
2023-12-06 06:58:24 +0000

    UPDATED

---
+++ 02_control_flow/7_reduce.js
@@ -0,0 +1,29 @@
+const myNums = [1,2,3]
+/*const myTotal = myNums.reduce( function(accumulator,currVal) {
+    console.log(`acc: ${accumulator} and current: ${currVal}`)
+    return accumulator + currVal
+},3)*/
+const myTotal = myNums.reduce ((acc,curr) => acc+curr,0)
+console.log(myTotal)
+
+const shoppingCart = [
+    {
+        itemName:"jsCourse",
+        price:299
+    },
+    {
+        itemName:"WebDev",
+        price:999
+    },
+    {
+        itemName:"AppDev",
+        price:1999
+    },
+    {
+        itemName:"DataAnalytics",
+        price:5299
+    },
+
+]
+const rpicetoPay = shoppingCart.reduce( (acc,item) => acc + item.price,0)
+console.log(rpicetoPay)
